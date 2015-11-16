29ed382916b7e436dd4353cbda19f3dfba14904d
Deric Bunch <ddbunc2@gmail.com>
2015-11-16 13:44:31 -0600

    added user auth and bootstrap-social/font-awesome

--- javascripts/dating-site.js
+++ javascripts/dating-site.js
@@ -5,17 +5,60 @@ require.config({
     'lodash': '../lib/bower_components/lodash/lodash.min',
     'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
     'q': '../lib/bower_components/q/q',
+    'firebase': '../lib/bower_components/firebase/firebase',
     'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min'
   },
   shim: {
-    'bootstrap': ['jquery']
+    'bootstrap': ['jquery'],
+    'firebase': {
+      exports: 'Firebase'
+    }
   }
 });
 
 require(
-  ["dependencies"], 
-  function(_$_) {
+  ["dependencies", "firebase", "auth"], 
+  function(_$_, firebase, auth) {
 
+    $('.btn-facebook').on('click', function(){
+      var ref = new Firebase("https://dog-dates.firebaseio.com/");
+      var authData = ref.getAuth();
+
+      if (authData === null) {
+        ref.authWithOAuthPopup("facebook", function(error, authData) {
+          if (error) {
+            console.log("Login Failed!", error);
+          } else {
+            console.log("Authenticated successfully with payload:", authData);
+            auth.setUid(authData.uid);
+          }
+        });
+      } else {
+        auth.setUid(authData.uid);
+      }
+      console.log(authData);
+    });
+
+    $('.btn-github').on('click', function(){
+      var ref = new Firebase("https://dog-dates.firebaseio.com/");
+      var authData = ref.getAuth();
+
+      if (authData === null) {
+        ref.authWithOAuthPopup("github", function(error, authData) {
+          if (error) {
+            console.log("Login Failed!", error);
+          } else {
+            console.log("Authenticated successfully with payload:", authData);
+            auth.setUid(authData.uid);
+           // require(["core_list"], function() {});
+          }
+        });
+      } else {
+        auth.setUid(authData.uid);
+        // require(["core_list"], function() {});
+      }
+      console.log(authData);
+    });
     /*
       You can choose to use the REST methods to interact with
       Firebase, or you can use the Firebase API with event
